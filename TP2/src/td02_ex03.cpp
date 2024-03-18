0f4c6b575fa21aee4e0b5ad87ee80645bc3954a3
anne passelegue <anne.passelegue@etu.esiee.fr>
2024-03-18 16:14:14 +0100

    TD 2 - exo 3 amélioration

--- TP2/src/td02_ex03.cpp
+++ TP2/src/td02_ex03.cpp
@@ -12,7 +12,7 @@ int main()
     std::vector<int> array { 41, 17, 34, 0, 19, 24, 28, 8, 12, 14, 5, 45, 31, 27, 11, 41, 45, 42, 27, 36, 41, 4, 2, 3, 42, 32, 21, 16, 18, 45, 47, 26, 21, 38, 19, 12, 17, 49, 35, 44, 3, 11, 22, 33, 23, 14, 41, 11, 3, 18 };
 
     {
-        // Calcul du temps d'exécution du tri par sélection
+        // Calcul du temps d'exécution du tri implémenté par C++
         ScopedTimer timer("Temps tri implemente par C++ : ");
         std::sort(array.begin(), array.end());  
     }
